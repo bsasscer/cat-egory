e4c571e2de8bf86e90c8479c1be55712f946a8c5
bsasscer <smithbi.maricela@gmail.com>
2019-04-12 09:03:32 -0700

    Updates

---
+++ precache-manifest.1508c240d2ba7f3eb316391c034ca6c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f54100b7b2e307ee894e",
+    "url": "/cat-egory/static/css/main.87162df2.chunk.css"
+  },
+  {
+    "revision": "f54100b7b2e307ee894e",
+    "url": "/cat-egory/static/js/main.9f82f5dd.chunk.js"
+  },
+  {
+    "revision": "378f06187431084d05a2",
+    "url": "/cat-egory/static/js/runtime~main.5e3d575d.js"
+  },
+  {
+    "revision": "59f1b4c56bc33d2c7153",
+    "url": "/cat-egory/static/js/2.43b610ba.chunk.js"
+  },
+  {
+    "revision": "30e9c4752a35f85ee292e00305ad0928",
+    "url": "/cat-egory/index.html"
+  }
+];
